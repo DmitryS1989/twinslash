c01f71450cfe073e41a12d3cf1f3e3f8d74a863f
DmitryS1989 <movs1989@gmail.com>
2022-01-10 12:19:40 +0300

    fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "twinslash_production"
 
-  cconfig.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false
   config.action_mailer.default_options = { from: 'devise@test.com' }
   config.action_mailer.default_url_options = { host: 'sheltered-beach-21528.herokuapp.com', protocol: 'https' }
   config.action_mailer.raise_delivery_errors = true
